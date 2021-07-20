df5ca2a026f3ff2e0d61aa6f1223ef98ad87c8d9
Jessica <Jessica_doyle@outlook.com>
2021-07-20 16:55:16 +0100

    Chapter 16 Complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,8 @@ require_relative "bike"
 
 class DockingStation
 
+  DEFAULT_CAPACITY = 20
+
   attr_reader :bike
 
   def initialize
@@ -21,7 +23,7 @@ class DockingStation
   private 
 
   def full?
-    return @bikes.count >= 20
+    return @bikes.count >= DEFAULT_CAPACITY
   end
 
   def bikes_empty?
